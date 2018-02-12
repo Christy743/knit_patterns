a288f986935a46fcbef6f2814b9128485b970861
Christina <github email address>
2018-02-12 14:06:58 -0700

    Updated omniauth in controllers and route.rb for the facebook omniauth. Still not getting the omniauth to work.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#welcome"
 
-  #get "/auth/facebook/callback" => "sessions#create"
+  #get "/login", :to => "sessions#new", :as => :login
+  #match "/auth/:provider/callback", :to => "sessions#create"
+  #match "/auth/failure", :to => "sessions#failure"
+
+  get "/auth/facebook/callback" => "sessions#create"
 
 
 end

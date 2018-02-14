be470051879b0eae297dbd366eec30be7d63ac42
Christina <github email address>
2018-02-13 21:21:06 -0700

    added custom methods to user.rb for logging in without using twitter omniauth.  changed views by adding a form to log in. the sign up is automatic through devise, but for some reason I had to set up a custom log in flow.  now I have to work on when I sign in either via twitter or custom that it signs in because it just sits there and has the message 'must sign in'.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,12 +4,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def sign_in_with(provider_name)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user, :event => :authentication
-    set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+    end
   end
 
   def twitter
     sign_in_with "Twitter"
+  end
+
+  def failure
+    redirect_to root_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,42 @@
-
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:twitter]
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :authentication_keys, presence: {email: true, login: false}
+  validate :validate_username
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  attr_accessor :login
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
     end
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase}]).first
+    else conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
 end
